9e89f6e6cb2a9f6163b3b86eefab486ab9d44dc8
Alehandro Maribona <maribonaa@yandex.ru>
2024-01-07 04:45:04 +0300

    wrote params for urlform

---
+++ yacut/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import URLField, SubmitField
+from wtforms.validators import DataRequired, Length, Optional, URL
+
+from settings import MAX_LENGTH_URL, MAX_LENGTH_SHORT_URL
+
+
+MESSAGE_REQUIRED = 'Обязательное поле'
+
+
+class UrlForm(FlaskForm):
+    original_link = URLField(
+        'Длинная ссылка',
+        validators=(
+            DataRequired(message=MESSAGE_REQUIRED),
+            URL(message='недопустимый URL-адрес'),
+            Length(
+                1, MAX_LENGTH_URL,
+                message=(
+                    f'Длина URL-адреса не может быть '
+                    f'больше {MAX_LENGTH_URL} символов'))
+        )
+    )
+    custom_id = URLField(
+        'Ваш вариант короткой ссылки',
+        validators=(
+            Optional(),
+            Length(
+                1, MAX_LENGTH_SHORT_URL,
+                message=(
+                    f'Длина URL-адреса не может быть '
+                    f'больше {MAX_LENGTH_SHORT_URL} символов'))
+        )
+    )
+    submit = SubmitField('Создать')
