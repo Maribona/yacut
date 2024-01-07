4559ae224e7a02519b7f8fb39a81c58fbb2f84c6
Alehandro Maribona <maribonaa@yandex.ru>
2024-01-07 04:03:57 +0300

    use constants for params

--- yacut/models.py
+++ yacut/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
 from yacut import db
+from settings import MAX_LENGTH_URL, MAX_LENGTH_SHORT_URL
 
 
 class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    original = db.Column(db.String(256), nullable=False)
-    short = db.Column(db.String(16), unique=True, nullable=False)
+    original = db.Column(db.String(MAX_LENGTH_URL), nullable=False)
+    short = db.Column(db.String(MAX_LENGTH_SHORT_URL), unique=True, nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
