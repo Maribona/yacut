edfe6861c6c8326aeee1798e080ad60329575b90
Alehandro Maribona <maribonaa@yandex.ru>
2024-01-07 17:52:01 +0300

    update for flake

--- yacut/models.py
+++ yacut/models.py
@@ -7,5 +7,6 @@ from settings import MAX_LENGTH_URL, MAX_LENGTH_SHORT_URL
 class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(MAX_LENGTH_URL), nullable=False)
-    short = db.Column(db.String(MAX_LENGTH_SHORT_URL), unique=True, nullable=False)
+    short = db.Column(db.String(
+        MAX_LENGTH_SHORT_URL), unique=True, nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
