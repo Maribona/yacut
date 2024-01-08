154ac23e6ac72a8c2240b24406feb050a4b97191
Alehandro Maribona <maribonaa@yandex.ru>
2024-01-08 23:39:25 +0300

    wrote func redirect_url_view

--- yacut/views.py
+++ yacut/views.py
@@ -1,8 +1,9 @@
 from flask import abort, flash, redirect, render_template, url_for
 
 from . import app, db
-from .forms import OpinionForm
-from .models import Opinion
+from yacut.forms import OpinionForm
+from yacut.models import URLMap
+from yacut.utils import get_unique_short_id
 
 
 @app.route('/', methods=('GET', 'POST'))
@@ -12,4 +13,5 @@ def main_view():
 
 @app.route('/<string:short_id>', methods=('GET',))
 def redirect_url_view(short_id):
-    pass
+    redirect_url = URLMap.query.filter_by(short=short_id).first_or_404()
+    return redirect(redirect_url.original)
