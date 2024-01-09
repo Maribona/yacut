1b82c00d26d10edf64c86b38cefb08c33e4dfa33
Alehandro Maribona <maribonaa@yandex.ru>
2024-01-09 17:26:09 +0300

    yacut/api_views.py

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,5 +1,5 @@
 from http import HTTPStatus as status
-from flask import jsonify
+from flask import jsonify, request
 from . import app, db
 
 from yacut.models import URLMap
@@ -8,11 +8,24 @@ from yacut.error_handlers import InvalidAPIUsage
 
 
 ERROR_NOTFOUND_ID = 'Указанная ссылка не найдена'
+ERROR_MISSING_FIELDS = 'В запросе отсутствует обязательное поле'
 
 
 @app.route('/api/id/', methods=('POST',))
 def create_short_url():
-    pass
+    data = request.get_json()
+    if not data:
+        raise InvalidAPIUsage(ERROR_MISSING_FIELDS)
+    if 'url' not in data:
+        raise InvalidAPIUsage(ERROR_MISSING_FIELDS)
+    if not data.get('custom_id'):
+        data['custom_id'] = get_unique_short_id()
+        raise InvalidAPIUsage('Данный вариант ссылки уже используется')
+    new_url = URLMap()
+    new_url.from_dict(data)
+    db.session.add(new_url)
+    db.session.commit()
+    return jsonify(new_url.to_dict()), status.CREATED
 
 
 @app.route('/api/id/<short_id>/', methods=('GET',))
