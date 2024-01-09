ed5057dab6310c4d2620a1e70e0fa5290e6ffac8
Alehandro Maribona <maribonaa@yandex.ru>
2024-01-09 12:44:40 +0300

    function main_view  was written

--- yacut/views.py
+++ yacut/views.py
@@ -1,4 +1,4 @@
-from flask import abort, flash, redirect, render_template, url_for
+from flask import flash, redirect, render_template, url_for
 
 from . import app, db
 from yacut.forms import UrlForm
@@ -6,9 +6,34 @@ from yacut.models import URLMap
 from yacut.utils import get_unique_short_id
 
 
+EXISTING_SHORT_LINK = 'Предложенный вариант короткой ссылки уже существует.'
+
+
 @app.route('/', methods=('GET', 'POST'))
 def main_view():
-    pass
+    form = UrlForm()
+    if form.validate_on_submit():
+        custom_id = form.custom_id.data
+        if URLMap.query.filter_by(short=custom_id).first():
+            flash(EXISTING_SHORT_LINK)
+            return render_template('index.html', form=form)
+        if not custom_id:
+            custom_id = get_unique_short_id()
+        short_url = URLMap(
+            original=form.original_link.data,
+            short=custom_id
+        )
+        db.session.add(short_url)
+        db.session.commit()
+        return render_template(
+            'index.html',
+            form=form,
+            short_link=url_for(
+                'redirect_url_view',
+                short_id=custom_id,
+                _external=True)
+        )
+    return render_template('index.html', form=form)
 
 
 @app.route('/<string:short_id>', methods=('GET',))
