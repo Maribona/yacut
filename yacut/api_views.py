b2ceebff3d85e722171b4e9e747d8dc4632f265b
Alehandro Maribona <maribonaa@yandex.ru>
2024-01-09 15:10:52 +0300

    function get_original_url was written

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,4 +1,10 @@
+from http import HTTPStatus as status
+from flask import jsonify
 from . import app, db
+
+from yacut.models import URLMap
+from yacut.utils import get_unique_short_id
+from yacut.error_handlers import InvalidAPIUsage
 
 
 @app.route('/api/id/', methods=('POST',))
@@ -7,5 +13,9 @@ def create_short_url():
 
 
 @app.route('/api/id/<short_id>/', methods=('GET',))
-def get_original_url():
-    pass
+def get_original_url(short_id):
+    original_url = URLMap.query.filter_by(short=short_id).first()
+    if original_url is not None:
+        data = original_url.to_dict()
+        return jsonify({'url': data['url']}), status.OK
+    raise InvalidAPIUsage('Указанный id не найден', status.NOT_FOUND)
