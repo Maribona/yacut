6b315744e20fa5e6beac370b126070f8156788c8
Alehandro Maribona <maribonaa@yandex.ru>
2024-01-14 17:34:31 +0300

    fixed bugs with db

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -7,7 +7,8 @@ from yacut.models import URLMap
 from yacut.utils import get_unique_short_id
 from yacut.error_handlers import InvalidAPIUsage
 
-from settings import MAX_LENGTH_SHORT_URL, REGEX_SHORT_URL
+from settings import MAX_LENGTH_SHORT_URL, REGEX_SHORT_URL, EXISTING_SHORT_LINK
+from urllib.parse import urljoin
 
 ERROR_NOTFOUND_ID = 'Указанный id не найден'
 ERROR_MISSING_FIELDS = 'В запросе отсутствует обязательное поле'
@@ -25,19 +26,21 @@ def create_short_url():
     if not custom_id or custom_id.strip() == '':
         custom_id = get_unique_short_id()
     elif URLMap.query.filter_by(short=custom_id).first() is not None:
-        raise InvalidAPIUsage(
-            'Предложенный вариант короткой ссылки уже существует.')
+        raise InvalidAPIUsage(EXISTING_SHORT_LINK)
     elif re.match(REGEX_SHORT_URL, custom_id) is None \
             or len(custom_id) > MAX_LENGTH_SHORT_URL:
         raise InvalidAPIUsage(
             'Указано недопустимое имя для короткой ссылки')
-
-    new_url = URLMap()
-    new_url.from_dict(data)
-    new_url.short = custom_id
+    new_url = URLMap(
+        original=data['url'],
+        short=custom_id)
     db.session.add(new_url)
     db.session.commit()
-    return jsonify(new_url.to_dict()), status.CREATED
+    return (
+        jsonify(
+            {'url': new_url.original,
+             'short_link': urljoin(request.url_root, custom_id)}
+        ), status.CREATED)
 
 
 @app.route('/api/id/<short_id>/', methods=('GET',))
