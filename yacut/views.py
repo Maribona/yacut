617b536ec0284ecb33e15b28110da283507f0ebe
Alehandro Maribona <maribonaa@yandex.ru>
2024-01-15 21:58:11 +0300

    fixed imports and added the Guard Block approach

--- yacut/views.py
+++ yacut/views.py
@@ -1,34 +1,34 @@
-from flask import flash, redirect, render_template, Response
+from flask import Response, flash, redirect, render_template
+from settings import EXISTING_SHORT_LINK
 
-from . import app, db
+from yacut import app, db
 from yacut.forms import UrlForm
 from yacut.models import URLMap
 from yacut.utils import get_unique_short_id
-from settings import EXISTING_SHORT_LINK
 
 
 @app.route('/', methods=('GET', 'POST'))
 def main_view() -> Response:
     form = UrlForm()
-    if form.validate_on_submit():
-        custom_id = form.custom_id.data
-        original_link = form.original_link.data
-        if URLMap.query.filter_by(short=custom_id).first():
-            flash(EXISTING_SHORT_LINK)
-            return render_template('yacut.html', form=form)
-        short_url = custom_id or get_unique_short_id()
-        new_url = URLMap(
-            original=original_link,
-            short=short_url
-        )
-        db.session.add(new_url)
-        db.session.commit()
-        return render_template(
-            'yacut.html',
-            form=form,
-            short_link=new_url.short
-        )
-    return render_template('yacut.html', form=form)
+    if not form.validate_on_submit():
+        return render_template('yacut.html', form=form)
+    custom_id = form.custom_id.data
+    original_link = form.original_link.data
+    if URLMap.query.filter_by(short=custom_id).first():
+        flash(EXISTING_SHORT_LINK)
+        return render_template('yacut.html', form=form)
+    short_url = custom_id or get_unique_short_id()
+    new_url = URLMap(
+        original=original_link,
+        short=short_url
+    )
+    db.session.add(new_url)
+    db.session.commit()
+    return render_template(
+        'yacut.html',
+        form=form,
+        short_link=new_url.short
+    )
 
 
 @app.route('/<string:short_id>', methods=('GET',))
